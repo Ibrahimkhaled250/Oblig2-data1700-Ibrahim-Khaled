67a5ba290a716d459d6c244bc9e8eef0a1312949
name <email>
2024-03-17 15:49:24 +0100

    Uppdating the First assigment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Oblig2-data1700-Ibrahim-Khaled
+
+=======
+OsloMet brukernavn: s366672
+
+GitHub brukernavn: Ibrahimkhaled250
+
+Github repo URL: https://github.com/Ibrahimkhaled250/Oblig2-data1700-Ibrahim-Khaled.git
+
+Fullt navn: Ibrahim Omar Khaled
+
+Kort beskrivelse av applikasjon (5-10 setninger):
---
+++ src/main/resources/static/Kun.js
@@ -0,0 +1,49 @@
+
+function kjøp(){
+    let KundeX = {
+        velgFilm: $("#velg").val(),
+        antall: $("#antall").val(),
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        tlf: $("#telefon").val(),
+        epost: $("#email").val()
+    }
+    $.post("/lagre", KundeX, function lagreKunde(){
+        tilbakeAlle();
+    });
+
+
+
+
+    $("#velg").val(""),
+        $("#antall").val(""),
+        $("#fornavn").val(""),
+        $("#etternavn").val(""),
+        $("#telefon").val(""),
+        $("#email").val("")
+
+}
+
+function tilbakeAlle() {
+    $.get("/sende", function (data) {
+        formaterData(data);
+    });
+}
+
+function formaterData(Kundene){
+    let ut = "<table class=\"table table-striped table-bordered\">" +
+        "<tr><th>Fornavn</th><th>Etternavn</th><th>Antall</th><th>Film</th><th>TelefonNr</th><th>Epost</th></tr>";
+    for (let enKunde of Kundene){
+        ut += "<tr><td>"+enKunde.fornavn+"</td><td>"+enKunde.etternavn+"</td><td>"+enKunde.antall+"</td><td>"+enKunde.velgFilm+"</td><td>"+enKunde.tlf+"</td><td>"+enKunde.epost+"</td></tr>";
+    }
+    ut += "</table>";
+    $("#resultat").html(ut);
+
+}
+
+
+function slett(){
+    $.get("/slett", function (){
+        tilbakeAlle();
+    });
+}
